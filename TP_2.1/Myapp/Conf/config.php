0fe3225020003e53e70a014379eecddf16eb0593
xuxiabing <mitoop@126.com>
2014-07-16 14:22:32 +0800

    2014
    @cnal

---
+++ TP_2.1/Myapp/Conf/config.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'APP_DEBUG' => true, // 开启调试模式
+    
+    'DB_TYPE'=> 'mysql',  
+
+	'DB_HOST'=> 'localhost',
+
+	'DB_NAME'=>'com_hmly',  
+
+	'DB_USER'=>'root', 
+
+	'DB_PWD'=>'tarena', 
+
+	'DB_PORT'=>'3306',
+
+	'DB_PREFIX'=>'',
+
+);
+?>
